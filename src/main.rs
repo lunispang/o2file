fc4046d2b6a72c91344720fbf728e03eaa9635b1
lunispang <linus.wennborn@outlook.com>
2024-11-18 00:38:29 +0100

    maybe progress

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::io::{BufReader, Read};
+use std::io::{BufReader, Read, Write};
 use std::net::*;
 
 const HTTP_OK: &str = "HTTP/1.1 200 OK\r\n";
 
 fn http_ok(content: &[u8]) -> Vec<u8> {
-    format!("{}Content-Length: {}\r\nContent-type: application/octet-stream\r\nContent-Disposition: attachment; filename=\"download\"\r\nConnection: close\r\n\r\n{}",
+    format!("{}Content-Length: {}\r\nContent-type: binary/octet-stream\r\nContent-Disposition: attachment; filename=\"download\"\r\nConnection: close\r\n\r\n{}",
         HTTP_OK, 
         content.len(), 
         unsafe { String::from_utf8_unchecked(content.to_vec()) }
@@ -16,8 +16,9 @@ fn stream_file(stream: &mut TcpStream, mut file: std::fs::File) -> Result<u64, s
     let _ = file.read_to_end(&mut vec);
     println!("{}", vec.len());
     let response = http_ok(&vec);
-    let mut file: BufReader<_> = BufReader::new(response.as_slice());
-    std::io::copy(&mut file, stream)
+    let r = std::io::copy(&mut response.as_slice(), stream);
+    let _ = stream.flush();
+    r    
 }
 
 //const HTTP_NOT_FOUND: &[u8] = b"HTTP/1.1 404 NOT FOUND\r\n\r\n";
@@ -26,12 +27,15 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     println!("Created TcpListener");
 
-    let filename: &str = &std::env::args().skip(1).next().expect("a filename");
+    let filename: String = std::env::args().skip(1).next().expect("a filename");
 
     for mut i in listener.incoming().filter_map(Result::ok) {
-        println!("Accepted a client");
-        let data = std::fs::File::open(filename).unwrap();
-        let r = stream_file(&mut i, data);
-        eprintln!("{r:?}");
+        let filename = filename.clone();
+        std::thread::spawn(move || {
+            println!("Accepted a client");
+            let data = std::fs::File::open(filename).unwrap();
+            let r = stream_file(&mut i, data);
+            eprintln!("{r:?}");
+        });
     }
 }
